8ec2fcb23f899b922bcfa1a9cdbbac5e88c9f894
Aldi2525 <tzzxuan1933@gmail.com>
2021-12-24 01:45:55 -0800

    adminlte

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,25 @@
-@extends('layouts.app')
+@extends('adminlte::page')
+
+@section('title','Dashboard')
+
+@section('content_header')
+
+Dashboard
+
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    @role('admin')
-                    Role Admin {{\Laratrust::hasRole('admin') }}
-                    @endrole
+Welcome to this beautiful admin panel.
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+
+
+@endsection
+
+@section('css')
+
+@endsection
+
+@section('js')
+
 @endsection
